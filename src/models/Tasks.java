0e5629189a6a35e72d1e1317e3e5e6e1907f34e3
hikaru-oshima <shonth2015@gmail.com>
2020-06-04 19:31:44 +0900

    Add getAllMessages

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -1,5 +1,4 @@
 package models;
-
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -7,25 +6,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTask",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "tasks")
-public class task {
-
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
 
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
-
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
 
     public Integer getId() {
         return id;
@@ -58,5 +64,6 @@ public class task {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
+
 
 }
